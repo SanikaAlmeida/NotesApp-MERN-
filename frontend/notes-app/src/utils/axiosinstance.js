e01e15cbfefc43dafdbcd49af0a41e38cd522815
SanikaAlmeida <142724093+SanikaAlmeida@users.noreply.github.com>
2025-05-28 20:36:51 +0530

    added hosted backend url

--- frontend/notes-app/src/utils/axiosinstance.js
+++ frontend/notes-app/src/utils/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosinstance= axios.create({
-  baseURL: 'http://localhost:8005',
+  baseURL: 'https://notesapp-mern-backend.onrender.com',
   timeout: 10000,
   headers: { 
     "Content-Type":"application/json"
